d866c835033eae43e287fba0e542df63ba408317
Viktor.Wiedmeier <Viktor.Wiedmeier@partner.bmw.de>
2018-10-24 13:57:40 +0200

    Kapitel 11: Movie Detail Screen (part 2)

--- app/src/main/java/com/example/viktor/moviemanager/models/Movie.java
+++ app/src/main/java/com/example/viktor/moviemanager/models/Movie.java
@@ -71,7 +71,7 @@ public class Movie implements Serializable {
     }
 
     public String getBackdropPath() {
-        return backdropPath;
+        return String.format("https://image.tmdb.org/t/p/w342%s",backdropPath);
     }
 
     public void setBackdropPath(String backdropPath) {
